e7ad6c0b49c4d72d597a76850b554efd5fc92133
Christoph Anton Mitterer <mail@christoph.anton.mitterer.name>
2020-05-01 01:31:16 +0200

    order files by test series
    Since I have multiple number of test series now, order each files belong to one
    by a leading number, so one can easier differentiate.
    
    1__ - the first series from my original post to LKML, repectively kernel bugzilla
    2__ - the second series, only on 5.5 with intel_pstate being enabled and only
          testing video playback with mpv

---
+++ 2__measure-all_mpv.sh
@@ -0,0 +1,61 @@
+if [ -z "$KERNEL_VERSION" -o -z "$DESKTOP_ENV" -o -z "$INTEL_PSTATE" ]; then
+	echo some env vars are not set
+	exit 2
+fi
+
+
+
+
+export LIBVA_DRIVER_NAME=i965 ; export M_GPU=gpu ; export M_HWDEC=no ; export M_RES=low ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+export LIBVA_DRIVER_NAME=i965 ; export M_GPU=gpu ; export M_HWDEC=no ; export M_RES=high ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+
+export LIBVA_DRIVER_NAME=i965 ; export M_GPU=gpu ; export M_HWDEC=vaapi ; export M_RES=low ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+export LIBVA_DRIVER_NAME=i965 ; export M_GPU=gpu ; export M_HWDEC=vaapi ; export M_RES=high ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+
+export LIBVA_DRIVER_NAME=i965 ; export M_GPU=gpu ; export M_HWDEC=vaapi-copy ; export M_RES=low ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+export LIBVA_DRIVER_NAME=i965 ; export M_GPU=gpu ; export M_HWDEC=vaapi-copy ; export M_RES=high ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+
+
+export LIBVA_DRIVER_NAME=i965 ; export M_GPU=vaapi ; export M_HWDEC=no ; export M_RES=low ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+export LIBVA_DRIVER_NAME=i965 ; export M_GPU=vaapi ; export M_HWDEC=no ; export M_RES=high ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+
+export LIBVA_DRIVER_NAME=i965 ; export M_GPU=vaapi ; export M_HWDEC=vaapi ; export M_RES=low ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+export LIBVA_DRIVER_NAME=i965 ; export M_GPU=vaapi ; export M_HWDEC=vaapi ; export M_RES=high ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+
+export LIBVA_DRIVER_NAME=i965 ; export M_GPU=vaapi ; export M_HWDEC=vaapi-copy ; export M_RES=low ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+export LIBVA_DRIVER_NAME=i965 ; export M_GPU=vaapi ; export M_HWDEC=vaapi-copy ; export M_RES=high ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+
+
+export LIBVA_DRIVER_NAME=i965 ; export M_GPU=xv ; export M_HWDEC=no ; export M_RES=low ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+export LIBVA_DRIVER_NAME=i965 ; export M_GPU=xv ; export M_HWDEC=no ; export M_RES=high ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+
+export LIBVA_DRIVER_NAME=i965 ; export M_GPU=xv ; export M_HWDEC=vaapi ; export M_RES=low ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+export LIBVA_DRIVER_NAME=i965 ; export M_GPU=xv ; export M_HWDEC=vaapi ; export M_RES=high ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+
+export LIBVA_DRIVER_NAME=i965 ; export M_GPU=xv ; export M_HWDEC=vaapi-copy ; export M_RES=low ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+export LIBVA_DRIVER_NAME=i965 ; export M_GPU=xv ; export M_HWDEC=vaapi-copy ; export M_RES=high ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+
+
+
+
+export LIBVA_DRIVER_NAME=iHD ; export M_GPU=gpu ; export M_HWDEC=no ; export M_RES=low ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+export LIBVA_DRIVER_NAME=iHD ; export M_GPU=gpu ; export M_HWDEC=no ; export M_RES=high ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+
+export LIBVA_DRIVER_NAME=iHD ; export M_GPU=gpu ; export M_HWDEC=vaapi ; export M_RES=low ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+export LIBVA_DRIVER_NAME=iHD ; export M_GPU=gpu ; export M_HWDEC=vaapi ; export M_RES=high ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+
+export LIBVA_DRIVER_NAME=iHD ; export M_GPU=gpu ; export M_HWDEC=vaapi-copy ; export M_RES=low ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+export LIBVA_DRIVER_NAME=iHD ; export M_GPU=gpu ; export M_HWDEC=vaapi-copy ; export M_RES=high ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+
+
+#anything with LIBVA_DRIVER_NAME=i965 and -vo=vaapi fails
+
+
+export LIBVA_DRIVER_NAME=iHD ; export M_GPU=xv ; export M_HWDEC=no ; export M_RES=low ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+export LIBVA_DRIVER_NAME=iHD ; export M_GPU=xv ; export M_HWDEC=no ; export M_RES=high ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+
+export LIBVA_DRIVER_NAME=iHD ; export M_GPU=xv ; export M_HWDEC=vaapi ; export M_RES=low ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+export LIBVA_DRIVER_NAME=iHD ; export M_GPU=xv ; export M_HWDEC=vaapi ; export M_RES=high ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+
+export LIBVA_DRIVER_NAME=iHD ; export M_GPU=xv ; export M_HWDEC=vaapi-copy ; export M_RES=low ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
+export LIBVA_DRIVER_NAME=iHD ; export M_GPU=xv ; export M_HWDEC=vaapi-copy ; export M_RES=high ; ./2__measure_mpv.sh $KERNEL_VERSION/$INTEL_PSTATE/$DESKTOP_ENV vadriver-${LIBVA_DRIVER_NAME}_vo-${M_GPU}_hwdec-${M_HWDEC}_res-${M_RES} --vo=${M_GPU} --hwdec=${M_HWDEC} ~calestyo/cpu-tests/test-vid-${M_RES}-res.mkv
